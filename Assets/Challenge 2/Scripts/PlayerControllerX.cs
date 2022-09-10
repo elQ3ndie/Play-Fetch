9e2456b9870882d689d6e15db823cce505f78a90
Andrew Miracle <andrewmiracle300@gmail.com>
2022-09-10 11:39:32 +0100

    Initial commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    //The number of dogs that can be spawned in a second is 2
+    private float dogSpawnTime = 0.5f;
+    private float timestamp;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Time.time >= timestamp && Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timestamp = Time.time + dogSpawnTime;
+        }
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Play-Fetch
+ In this game, a dog is sent to fetch the ball before it drops to the ground 
